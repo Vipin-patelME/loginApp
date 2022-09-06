11a1943289632bb8731e5108600f9cb69d43b8a7
vipin-patelME <t4t.forex@gmail.com>
2022-09-06 12:45:22 +0000

    practice

---
+++ src/components/Home/index.js
@@ -0,0 +1,54 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Home extends Component {
+  state = {isLogin: true}
+
+  onLogin = () => {
+    this.setState(prevState => {
+      console.log(prevState)
+      return {isLogin: false}
+    })
+  }
+
+  onLoguot = () => {
+    this.setState(prevState => {
+      console.log(prevState)
+      return {isLogin: true}
+    })
+  }
+
+  render() {
+    const {isLogin} = this.state
+
+    let loginCont
+    if (isLogin) {
+      loginCont = (
+        <div className="bg-container">
+          <div>
+            <h1>Welcome User</h1>
+            <button className="btnStyle" type="button" onClick={this.onLogin}>
+              Logout
+            </button>
+          </div>
+        </div>
+      )
+    } else {
+      loginCont = (
+        <div className="bg-container">
+          <div>
+            <h1>Please login</h1>
+            <button className="btnStyle" type="button" onClick={this.onLoguot}>
+              Login
+            </button>
+          </div>
+        </div>
+      )
+    }
+
+    return <div> {loginCont}</div>
+  }
+}
+
+export default Home
